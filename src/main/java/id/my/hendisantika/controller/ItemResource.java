916cb7f22c8625c478d50951de5641f9289b7437
hendisantika <hendisantika@yahoo.co.id>
2025-02-22 08:32:17 +0700

    feat: Add ItemResource🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/controller/ItemResource.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.controller;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-crud-postgres
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 22/02/25
+ * Time: 08.31
+ * To change this template use File | Settings | File Templates.
+ */
+@Path("/items")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ItemResource {
+}
